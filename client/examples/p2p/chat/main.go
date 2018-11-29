93210b4a4deb6c7478a5543428d385e1c74d63e5
obrienthomas13 <obrienthomas13@gmail.com>
2018-11-29 13:44:03 -0800

    Fiddling with stuff

--- client/examples/p2p/chat/main.go
+++ client/examples/p2p/chat/main.go
@@ -26,7 +26,7 @@
  *
  */
 
-package main
+package chat
 
 import (
 	"bufio"
@@ -305,7 +305,7 @@ func (c *Client) writeExampleData(s net.Stream) {
 
 // }
 
-func main() {
+func (c *Client) BeginStream() {
 	fmt.Println("START OF MAIN FUNCTION")
 	sourcePort := flag.Int("sp", 0, "Source port number")
 	dest := flag.String("d", "", "Destination multiaddr string")
@@ -454,3 +454,153 @@ func main() {
 		select {}
 	}
 }
+
+// func main() {
+// 	fmt.Println("START OF MAIN FUNCTION")
+// 	sourcePort := flag.Int("sp", 0, "Source port number")
+// 	dest := flag.String("d", "", "Destination multiaddr string")
+// 	help := flag.Bool("help", false, "Display help")
+// 	debug := flag.Bool("debug", false, "Debug generates the same node ID on every execution")
+// 	// testMap := make(map[string]string)
+// 	sampleClient := &Client{}
+
+// 	flag.Parse()
+
+// 	if *help {
+// 		// fmt.Printf("This program demonstrates a simple p2p chat application using libp2p\n\n")
+// 		fmt.Println("Usage: Run './chat-exec -sp <SOURCE_PORT>' where <SOURCE_PORT> can be any port number.")
+// 		fmt.Println("Now run './chat-exec -d <MULTIADDR>' where <MULTIADDR> is multiaddress of previous listener host.")
+
+// 		os.Exit(0)
+// 	}
+
+// 	// If debug is enabled, use a constant random source to generate the peer ID. Only useful for debugging,
+// 	// off by default. Otherwise, it uses rand.Reader.
+// 	var r io.Reader
+// 	if *debug {
+// 		// Use the port number as the randomness source.
+// 		// This will always generate the same host ID on multiple executions, if the same port number is used.
+// 		// Never do this in production code.
+// 		r = mrand.New(mrand.NewSource(int64(*sourcePort)))
+// 	} else {
+// 		r = rand.Reader
+// 	}
+
+// 	// Creates a new RSA key pair for this host.
+// 	prvKey, _, err := crypto.GenerateKeyPairWithReader(crypto.RSA, 2048, r)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	// 0.0.0.0 will listen on any interface device.
+// 	sourceMultiAddr, _ := multiaddr.NewMultiaddr(fmt.Sprintf("/ip4/0.0.0.0/tcp/%d", *sourcePort))
+
+// 	// libp2p.New constructs a new libp2p Host.
+// 	// Other options can be added here.
+// 	host, err := libp2p.New(
+// 		context.Background(),
+// 		libp2p.ListenAddrs(sourceMultiAddr),
+// 		libp2p.Identity(prvKey),
+// 	)
+
+// 	sampleClient.host = host
+// 	// sampleClient.streams = make(map[peer.ID]net.Stream)
+// 	// sampleClient.rw = make(map[peer.ID]*bufio.ReadWriter)
+// 	sampleClient.streams = make(map[net.Stream]net.Stream)
+// 	sampleClient.rw = make(map[net.Stream]*bufio.ReadWriter)
+
+// 	// fmt.Println("HOST.ID().PRETTY()")
+// 	// fmt.Println("HOST.ID().PRETTY()")
+// 	// fmt.Printf("%+v\n", host.ID().Pretty())
+// 	// fmt.Println("HOST.ID().PRETTY()")
+// 	// fmt.Println("HOST.ID().PRETTY()")
+
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	if *dest == "" {
+// 		// Set a function as stream handler.
+// 		// This function is called when a peer connects, and starts a stream with this protocol.
+// 		// Only applies on the receiving side.
+// 		host.SetStreamHandler("/chat/1.0.0", sampleClient.handleStream)
+
+// 		// Let's get the actual TCP port from our listen multiaddr, in case we're using 0 (default; random available port).
+// 		var port string
+// 		for _, la := range host.Network().ListenAddresses() {
+// 			if p, err := la.ValueForProtocol(multiaddr.P_TCP); err == nil {
+// 				port = p
+// 				break
+// 			}
+// 		}
+
+// 		if port == "" {
+// 			panic("was not able to find actual local port")
+// 		}
+
+// 		fmt.Printf("Run './chat-exec -d /ip4/127.0.0.1/tcp/%v/p2p/%s' on another console.\n", port, host.ID().Pretty())
+// 		fmt.Println("You can replace 127.0.0.1 with public IP as well.")
+// 		fmt.Printf("\nWaiting for incoming connection\n\n")
+
+// 		// Hang forever
+// 		<-make(chan struct{})
+// 	} else {
+// 		fmt.Println("This node's multiaddresses:")
+// 		for _, la := range host.Addrs() {
+// 			fmt.Printf(" - %v\n", la)
+// 		}
+// 		fmt.Println()
+
+// 		// Turn the destination into a multiaddr.
+// 		maddr, err := multiaddr.NewMultiaddr(*dest)
+// 		if err != nil {
+// 			log.Fatalln(err)
+// 		}
+
+// 		// Extract the peer ID from the multiaddr.
+// 		info, err := peerstore.InfoFromP2pAddr(maddr)
+// 		if err != nil {
+// 			log.Fatalln(err)
+// 		}
+
+// 		host.SetStreamHandler("/chat/1.0.0", sampleClient.handleStream)
+// 		fmt.Printf("Run './chat-exec -d /ip4/127.0.0.1/tcp//p2p/%s' on another console.\n", host.ID().Pretty())
+
+// 		// Add the destination's peer multiaddress in the peerstore.
+// 		// This will be used during connection and stream creation by libp2p.
+// 		host.Peerstore().AddAddrs(info.ID, info.Addrs, peerstore.PermanentAddrTTL)
+
+// 		// Start a stream with the destination.
+// 		// Multiaddress of the destination peer is fetched from the peerstore using 'peerId'.
+// 		s, err := host.NewStream(context.Background(), info.ID, "/chat/1.0.0")
+// 		if err != nil {
+// 			panic(err)
+// 		}
+
+// 		// Create a buffered stream so that read and writes are non blocking.
+// 		rw := bufio.NewReadWriter(bufio.NewReader(s), bufio.NewWriter(s))
+
+// 		sampleClient.testMap = make(map[string]string)
+// 		sampleClient.rw[s] = rw
+
+// 		// sampleClient := &Client{
+// 		// 	testMap: make(map[string]string),
+// 		// 	rw:      rw,
+// 		// }
+
+// 		// testMap := make(map[string]string)
+
+// 		go sampleClient.readExampleData(s)
+// 		go sampleClient.writeExampleData(s)
+
+// 		// fmt.Println("LET'S CHECK OUT THOSE STREAMS")
+// 		// fmt.Println("%+v\n", sampleClient.streams)
+
+// 		// Create a thread to read and write data.
+// 		// go writeData(rw)
+// 		// go readData(rw)
+
+// 		// Hang forever.
+// 		select {}
+// 	}
+// }
