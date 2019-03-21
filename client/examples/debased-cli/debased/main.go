4cfe165b6e9af0d8b1479ba1428354d215620a08
JacksonJW <jacksonjwatkins@gmail.com>
2019-03-20 18:33:48 -0700

    Did some restructuring of the folder. Added a new command - createAcct to flesh out the --help flag

--- client/examples/debased-cli/debased/main.go
+++ client/examples/debased-cli/debased/main.go
@@ -14,7 +14,7 @@
 
 package main
 
-import "github.com/JohnGoocher/debased/client/examples/debased-cli/cmd"
+import "github.com/JohnGoocher/debased/client/examples/debased-cli/debased/cmd"
 
 func main() {
 	cmd.Execute()
